ddc83b9ac5521707ee1ec15018ba9e9a358710be
Ali Gadimi <aligadimi@outlook.com>
2019-11-26 00:30:06 +0330

    Initial commit

---
+++ app/src/main/java/com/agadimi/imageviewer/MainActivity.java
@@ -0,0 +1,45 @@
+package com.agadimi.imageviewer;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ImageView sceneIv;
+
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.activity_fade_in, R.anim.activity_fade_out);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+
+        if (getIntent() != null && getIntent().getData() != null)
+        {
+            Uri uri = getIntent().getData();
+            sceneIv.setImageURI(uri);
+
+            Animation animation = AnimationUtils.loadAnimation(this, R.anim.scene_enter);
+            sceneIv.startAnimation(animation);
+        }
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.activity_fade_in, R.anim.activity_fade_out);
+    }
+
+    private void bindViews()
+    {
+        sceneIv = findViewById(R.id.scene_iv);
+    }
+}
