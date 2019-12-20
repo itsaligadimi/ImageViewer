60acf4a489e1665f6307ba54a47857d2b2e6a513
Ali Gadimi <aligadimi@outlook.com>
2019-12-20 15:02:16 +0330

    cm

--- app/src/main/java/com/agadimi/imageviewer/MainActivity.java
+++ app/src/main/java/com/agadimi/imageviewer/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -28,6 +29,10 @@ public class MainActivity extends AppCompatActivity
 
             Animation animation = AnimationUtils.loadAnimation(this, R.anim.scene_enter);
             sceneIv.startAnimation(animation);
+        }
+        else
+        {
+            finish();
         }
     }
 
